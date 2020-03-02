3192b8e180615c94d7806046847d9f7694a49a42
Robert Isoski <r@robiso.com>
2020-03-02 21:19:52 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -13,11 +13,11 @@
 		<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<?= $Wcms->css() ?>
 		<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<?= $Wcms->css() ?>
 
 	</head>
 	
