a0fa0dce51bccb45e600832a5dd6bc8c7cec792f
Robert Isoski <r@robiso.com>
2021-12-31 00:09:50 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -13,13 +13,14 @@
 		<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
-
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+		<!-- Admin CSS -->
 		<?= $Wcms->css() ?>
 
-	</head>
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
+   	 </head>
 	
 	<body>
 	    <div class="overlay"></div>
